70d4ee37dd0ec073dd559302cbfa6bdffcb3b356
HARSH646464 <harshtomar4081@gmail.com>
2025-08-24 09:50:46 +0530

    Add files via upload

---
+++ personalized_greeting.py
@@ -0,0 +1,4 @@
+first_name=input('Enter tour first name: ')
+Last_name=input('Enter your last name: ')
+full_name=first_name +' '+ Last_name
+print('Hello,',full_name+'!',' Welcome to python program.')
